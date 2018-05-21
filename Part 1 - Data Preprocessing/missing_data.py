10208ad5b6e1f77586c3892287700608636282c9
albertopastormr <general.tyrus@hotmail.com>
2018-05-21 20:36:55 +0200

    update missing data py error

--- Part 1 - Data Preprocessing/missing_data.py
+++ Part 1 - Data Preprocessing/missing_data.py
@@ -1,17 +1,15 @@
-# Data Preprocessing
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
-
-# Taking care of missing data
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+dataset = pd.read_csv('Data.csv')
+
+X = dataset.iloc[:, :-1]
+Y = dataset.iloc[:, 3]
+
+# Fixing the missing data fields
+# iloc --> Integer indexing ; loc --> tag-row,tag-column types indexing
+impute = Imputer(missing_values= 'NaN', strategy='mean', axis=0)
+impute = impute.fit(X.iloc[:, 1:3])
+X.iloc[:, 1:3] = impute.transform(X.iloc[:, 1:3])
